d6881c187c6843b77ca854a1aeb6100f855640a6
RosanaGarciap <97164329+RosanaGarciap@users.noreply.github.com>
2022-04-04 14:22:30 -0400

    Add files via upload

---
+++ game/handler/score_handler.py
@@ -0,0 +1,18 @@
+
+class Score_handler():
+
+    def __init__(self) -> None:
+        self.__score = 0
+        self.__life = 3
+    
+    def increase_score(self, value):
+        self.__score = self.__score + value
+
+    def decrease_score(self, value):
+        self.__score = self.__score - value
+
+    def reduce_life(self):
+        self.__life = self.__life -1
+    
+    def get_score(self):
+        return str(self.__score)
